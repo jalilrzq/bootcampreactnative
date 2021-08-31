f3be4a0637506ed31334fb5d3d697815da67dfc5
Abdul Jalil <jalilrzq@gmail.com>
2021-08-31 22:34:10 +0700

    Tugas 12 - React Native - Component

---
+++ SanberApp/Tugas/Tugas12/dummy.js
@@ -0,0 +1,28 @@
+const Data = [
+    {
+        id : "1",
+        image: require("./asset/profile1.png"),
+        name:'Achmad Hilmy' ,
+        message:'Hello Achmad Hilmy',
+        time:'12.00',
+        totalMessage:'2'
+    },
+    {
+        id : "2",
+        image: require("./asset/profile2.png"),
+        name:'Deddy Corbuzier' ,
+        message:'Hello Deddy Corbuzier',
+        time:'13.02',
+        totalMessage:'12'
+    },
+    {
+        id : "3",
+        image: require("./asset/profile3.png"),
+        name:'Bobon Santoso' ,
+        message:'Hello Bobon',
+        time:'12.00',
+        totalMessage:'3'
+    }
+]
+
+export { Data }
